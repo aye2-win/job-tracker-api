1e96acb7a4ba026e1d27b9409d1e476be5280def
aye2-win <ayyayywin02@gmail.com>
2026-08-19 11:24:10 +0630

    add database connection

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middleware
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
+
+// start server
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+});
